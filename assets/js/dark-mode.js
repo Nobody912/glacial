5feb7c0dd4866e66db1b608f387d12b12777a5b0
Nobody912 <41035180+Nobody912@users.noreply.github.com>
2020-07-22 01:21:06 -0700

    tired, fix tmrw

--- assets/js/dark-mode.js
+++ assets/js/dark-mode.js
@@ -1,4 +1,13 @@
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
 
 function switchTheme(e) {
     if (e.target.checked) {
