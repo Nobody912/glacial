b3107b43a1480164b71675b3b23b9af135917528
Nobody912 <41035180+Nobody912@users.noreply.github.com>
2020-07-22 01:29:01 -0700

    clean up 2

--- assets/js/dark-mode.js
+++ assets/js/dark-mode.js
@@ -1,9 +1,9 @@
-const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 const currentTheme = localStorage.getItem('theme');
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
-    
+
     if (currentTheme === 'dark') {
         toggleSwitch.checked = true;
     }
